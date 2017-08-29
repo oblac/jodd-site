989cb1d71aaab8c9dbbb442d15d83c4bda5c3792
Igor Spasić <igor@jodd.org>
2017-08-30 01:01:09 +0200

    nanoocing nanoc

--- lib/js_meta_data.rb
+++ lib/js_meta_data.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 require 'json'
-require 'fileutils'
+require 'kramdown'
 
 module JoddJsMeta
 
@@ -34,57 +34,45 @@ module JoddJsMeta
 
 
   def js_meta_data(params = {})
-    print ("Generating JS search data...\n")
+    print ("Generating JS meta-data...\n")
     # Extract parameters
     items       = params.fetch(:items) { @items.reject { |i| i[:is_hidden] } }
-    select_proc = params.fetch(:rep_select, nil)
 
-    # Create builder
     buffer = "["
     count = 0
 
-    # Build sitemap
-    # Add item
     items.sort_by(&:identifier).each do |item|
-      reps = item.reps.select(&:path)
-      reps.select! { |r| select_proc[r] } if select_proc
-      reps.sort_by { |r| r.name.to_s }.each do |rep|
-        if (rep.path).end_with?(".html")
 
-          begin
-            content = File.read("output" + rep.path)
-          rescue SystemCallError
-            print ("\tSkipped " + rep.path)
-            return
-          end
-          html = Nokogiri::HTML(content)
+      if (item.identifier.to_s).end_with?(".md")
+        content = item.raw_content
+        html = Kramdown::Document.new(content).to_html
+        html = Nokogiri::HTML(html)
 
-          document = {
-            :title => @item[:title]? @item[:title] : 'Jodd',
-            :alltitles => extract_text2(html, '//*[self::h1 or self::h2]/text()'),
-            :body => extract_text(html, '//*/text()'),
-          }
+        document = {
+          :title => item[:title]? item[:title] : 'Jodd',
+          :alltitles => extract_text2(html, '//*[self::h1 or self::h2]/text()'),
+          :body => extract_text(html, '//*/text()'),
+        }
 
-          text = document[:body] <<
-            ' ' << document[:title] <<
-            ' ' << document[:title] <<
-            ' ' << document[:title] <<
-            ' ' << document[:title] <<
-            ' ' << document[:title] <<
-            ' ' << document[:alltitles] <<
-            ' ' << document[:alltitles]
+        text = document[:body] <<
+          ' ' << document[:title] <<
+          ' ' << document[:title] <<
+          ' ' << document[:title] <<
+          ' ' << document[:title] <<
+          ' ' << document[:title] <<
+          ' ' << document[:alltitles] <<
+          ' ' << document[:alltitles]
 
-          words = word_counter(text, 15)
-          words = words.map{|e| e[0] }.join(' ')
+        words = word_counter(text, 15)
+        words = words.map{|e| e[0] }.join(' ')
 
-          json = {"t" => document[:title],"v" => words,"u"=>@item[:site_path]}
+        json = {"t" => document[:title],"v" => words,"u"=>item[:site_path]}
 
-          if count != 0
-            buffer = buffer + ','
-          end
-          count += 1
-          buffer = buffer + JSON.pretty_generate(json)
+        if count != 0
+          buffer = buffer + ','
         end
+        count += 1
+        buffer = buffer + JSON.pretty_generate(json)
       end
     end
 
